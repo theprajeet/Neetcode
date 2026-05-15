aae84f5a354cd8d5ce26beb43ee0cf081d44b78d
Prajeet K Hiremath <116144821+theprajeet@users.noreply.github.com>
2026-05-15 23:21:17 +0530

    Add: isomorphic-strings - submission-0

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isIsomorphic(s, t) {
+    let obj = {};
+
+    for (let i = 0; i < s.length; i++) {
+        if (obj[s[i]]) {
+            if (obj[s[i]] !== t[i]) {
+                return false;
+            }
+        } 
+        else {
+            for (let key in obj) {
+                if (obj[key] === t[i]) {
+                    return false;
+                }
+            }
+            obj[s[i]] = t[i];
+        }
+    }
+    return true; 
+    }
+}
